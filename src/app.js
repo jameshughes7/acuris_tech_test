32355cb83f0c3cf1ad45faa4988e9460fc7ec707
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-11-27 12:09:49 +0000

    Add first mongoDb request

--- src/app.js
+++ src/app.js
@@ -10,6 +10,15 @@ app.set('view engine', 'handlebars');
 var MongoClient = require('mongodb').MongoClient;
 
 app.get('/open/:id', (req, res) => {
+  MongoClient.connect('mongodb://mm_recruitment_user_readonly:rebelMutualWhistle@ds037551.mongolab.com:37551/mm-recruitment', function(err, db) {
+    if(err) throw err
+    db.collection('company', function(err, collection) {
+      collection.find().toArray(function(err, items) {
+          if(err) throw err
+          console.log(items)
+      });
+    })
+  })
   res.render('main');
 });
 
