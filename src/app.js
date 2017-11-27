260438c45922f05482a2ede9999c60fe75e4a2ce
oleglukyanov <oleg.s.lukyanov@gmail.com>
2017-11-27 12:45:46 +0000

    Add Company template and display a company tickerCode in it

--- src/app.js
+++ src/app.js
@@ -9,17 +9,19 @@ app.set('view engine', 'handlebars');
 
 var MongoClient = require('mongodb').MongoClient;
 
-app.get('/open/:id', (req, res) => {
-  MongoClient.connect('mongodb://mm_recruitment_user_readonly:rebelMutualWhistle@ds037551.mongolab.com:37551/mm-recruitment', function(err, db) {
-    if(err) throw err
-    db.collection('company', function(err, collection) {
-      collection.find().toArray(function(err, items) {
-          if(err) throw err
-          console.log(items)
-      });
-    })
-  })
-  res.render('main');
+app.get('/show/:tickerCode', (req, res) => {
+
+
+  // MongoClient.connect('mongodb://mm_recruitment_user_readonly:rebelMutualWhistle@ds037551.mongolab.com:37551/mm-recruitment', function(err, db) {
+  //   if(err) throw err
+  //   db.collection('company', function(err, collection) {
+  //     collection.find().toArray(function(err, items) {
+  //         if(err) throw err
+  //         console.log(items)
+  //     });
+  //   })
+  // })
+  res.render('company', {tickerCode: req.params.tickerCode});
 });
 
 app.get('/', (req, res) => {
